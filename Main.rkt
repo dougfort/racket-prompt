43084b615cf3fc5029e5fd0648eb8ac1f58c7f6b
dougfort <doug.fort@gmail.com>
2023-05-05 14:01:25 -0400

    split out the chat module

--- Main.rkt
+++ Main.rkt
@@ -2,41 +2,6 @@
 
 ;; 
 
-(require net/http-client)
-(require json)
+(require "chat.rkt")
 
-;; looking for '("HTTP/1.1" "200" "OK")
-;; returning (status reason)
-(define (parse-status-line status-line)
-  (let ([parts ((compose1 string-split bytes->string/utf-8) status-line)])
-    (values ((compose string->number second) parts) (string-join (drop parts 2)))))
-
-;; API entry point
-(define (create-uri)
-  (string-append "/v1/chat/completions"))
-
-;; API Key
-(define (load-authorization api-key)
-  (string-append "Bearer " api-key))
-
-;; return query results
-(define (query api-key)
-  (let-values ([(status-line header-list data-port)
-                (http-sendrecv
-                 "api.openai.com"
-                 (create-uri)
-                 #:ssl? #t
-                 #:method #"POST"
-                 #:headers (list
-                            (string-append "Authorization: " (load-authorization api-key))
-                            "Content-Type: application/json")
-                 #:data #"{
- \"model\": \"gpt-3.5-turbo\",
- \"messages\": [{\"role\": \"user\", \"content\": \"What is the OpenAI mission?\"}]
-}")])
-    (let-values ([(status reason) (parse-status-line status-line)])
-      (unless (= 200 status)
-        (error (format "invalid HTTP status ~s; ~s" status reason))))
-    (let ([result-hash (read-json data-port)])
-      (print result-hash)
-      (hash-keys result-hash))))
+(define content "What is the OpenAI mission?")
---
+++ chat.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+;; 
+
+(require net/http-client)
+(require json)
+
+(provide query)
+
+;; looking for '("HTTP/1.1" "200" "OK")
+;; returning (status reason)
+(define (parse-status-line status-line)
+  (let ([parts ((compose1 string-split bytes->string/utf-8) status-line)])
+    (values ((compose string->number second) parts) (string-join (drop parts 2)))))
+
+;; API entry point
+(define (create-uri)
+  (string-append "/v1/chat/completions"))
+
+;; API Key
+(define (load-authorization api-key)
+  (string-append "Bearer " api-key))
+
+(define (query-hash query-content)
+  (hash
+   'model "gpt-3.5-turbo"
+   'messages (list (hash
+                    'role "user"
+                    'content query-content))))                      
+
+;; return query results
+(define (query api-key content)
+  (let-values ([(status-line header-list data-port)
+                (http-sendrecv
+                 "api.openai.com"
+                 (create-uri)
+                 #:ssl? #t
+                 #:method #"POST"
+                 #:headers (list
+                            (string-append "Authorization: " (load-authorization api-key))
+                            "Content-Type: application/json")
+                 #:data (jsexpr->string (query-hash content)))])
+    (let-values ([(status reason) (parse-status-line status-line)])
+      (unless (= 200 status)
+        (error (format "invalid HTTP status ~s; ~s" status reason))))
+    (read-json data-port)))
